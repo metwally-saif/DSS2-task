5c6fc64d54e3cf031c260419d4c24df38b7a359c
Saif Ayman Fathi Mohamed Metwally <65094581+metwally-saif@users.noreply.github.com>
2024-04-19 12:45:30 +0300

    last

---
+++ Forum.Infrastructure/Migrations/20240418171812_Initial.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Forum.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    username = table.Column<string>(type: "TEXT", maxLength: 64, nullable: false),
+                    email = table.Column<string>(type: "TEXT", maxLength: 128, nullable: false),
+                    password = table.Column<string>(type: "TEXT", maxLength: 32, nullable: false),
+                    first_name = table.Column<string>(type: "TEXT", maxLength: 64, nullable: true),
+                    last_name = table.Column<string>(type: "TEXT", maxLength: 256, nullable: true),
+                    role = table.Column<string>(type: "TEXT", nullable: false, defaultValue: "User"),
+                    is_deleted = table.Column<bool>(type: "INTEGER", nullable: false, defaultValue: false),
+                    create_date = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    update_date = table.Column<DateTime>(type: "timestamp", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "topic",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Creator = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false),
+                    CreatorId = table.Column<long>(type: "INTEGER", nullable: false),
+                    subject = table.Column<string>(type: "TEXT", maxLength: 256, nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false),
+                    Likes = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<long>(type: "INTEGER", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "timestamp", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "timestamp", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_topic", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_topic_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "comment",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Likes = table.Column<int>(type: "INTEGER", nullable: false),
+                    Text = table.Column<string>(type: "TEXT", nullable: false),
+                    Status = table.Column<int>(type: "INTEGER", nullable: false),
+                    Creator = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatorId = table.Column<long>(type: "INTEGER", nullable: true),
+                    TopicId = table.Column<long>(type: "INTEGER", nullable: true),
+                    UserId = table.Column<long>(type: "INTEGER", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_comment", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_comment_topic_TopicId",
+                        column: x => x.TopicId,
+                        principalTable: "topic",
+                        principalColumn: "id");
+                    table.ForeignKey(
+                        name: "FK_comment_users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "users",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_comment_TopicId",
+                table: "comment",
+                column: "TopicId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_comment_UserId",
+                table: "comment",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_topic_UserId",
+                table: "topic",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_public_users_usename_role",
+                table: "users",
+                columns: new[] { "username", "role" });
+
+            migrationBuilder.CreateIndex(
+                name: "UX_public_users_email",
+                table: "users",
+                column: "email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "UX_public_users_username",
+                table: "users",
+                column: "username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "comment");
+
+            migrationBuilder.DropTable(
+                name: "topic");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
